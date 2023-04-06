ff820dd931bd303e8a863b83f67b016103e61acb
luwbo1003 <Uyenchip1003@gmail.com>
2023-04-06 23:17:04 +0700

    update

--- app/src/main/java/com/example/group7/activities/MainActivity.java
+++ app/src/main/java/com/example/group7/activities/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.fragment.app.FragmentManager;
 
-//import android.app.FragmentManager;
 import android.os.Bundle;
 
 import com.example.group7.R;
@@ -13,6 +12,8 @@ import com.example.group7.databinding.ActivityMainBinding;
 import com.example.group7.UI.Fragment.CartFragment;
 import com.example.group7.UI.Fragment.HomeFragment;
 import com.example.group7.UI.Fragment.UserFragment;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +21,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("alo tui nè");
+        //////////////////////////////////
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
